fb2285e7be69cc2d3f80b15a7e39e404dce6db1a
Justin Schaaf <justinhschaaf@outlook.com>
2024-01-28 18:35:10 -0800

    Partially revert changes from eca39e3
    Return to using fetchTarball instead of fetchGit

--- default.nix
+++ default.nix
@@ -1,8 +1,7 @@
 # https://discourse.nixos.org/t/fixing-error-attribute-currentsystem-missing-in-flake/22386/7
-{ system ? "x86_64-linux", pkgs ? import (fetchGit {
-    url = "https://github.com/NixOS/nixpkgs.git";
-    ref = "refs/heads/nixos-unstable";
-    rev = "9b19f5e77dd906cb52dade0b7bd280339d2a1f3d";
+{ system ? "x86_64-linux", pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/9b19f5e77dd906cb52dade0b7bd280339d2a1f3d.tar.gz";
+    sha256 = "0939vbhln9d33xkqw63nsk908k03fxihj85zaf70i3il9z42q8mc";
 }) { inherit system; }, ... }:
 
 with pkgs;
